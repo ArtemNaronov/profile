d0840c12c06769d25fc19e9fd6f5b51d89dd0690
ArtemNaronov <a.naronov@bk.ru>
2023-10-03 00:07:21 +0400

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  devtools: { enabled: false },
   css: ['~/assets/scss/main.scss'],
-  alias: {
-    assets: "/<rootDir>/assets"
-  },
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "@/assets/scss/_colors.scss" as *;'
+          additionalData: '@import "@/assets/scss/_colors.scss";',
         }
       }
     },
